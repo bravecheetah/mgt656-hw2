c0692d854f8ddfb81bd937f71f2a1140b87db6c2
Michael Linares <michael.linares@yale.edu>
2015-01-27 17:00:13 +0000

    add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am a complete ninja now.');
+});
+
+app.get('/netid', function(request, response){
+    response.send('mel63');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('brave-cheetah');
 });
 
 app.listen(process.env.PORT || 4000);
